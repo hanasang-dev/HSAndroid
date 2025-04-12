38f00d40494861c56bed9dcc702a64068da2b44b
kosanghan <ko20091449@gmail.com>
2025-04-12 10:53:17 +0900

    build: apply Gradle Build Scan with Develocity plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,17 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "HSAndroid"
+
+plugins{
+    id("com.gradle.develocity") version("4.0")
+}
+
+develocity {
+    buildScan {
+        termsOfService {
+            url.set("https://gradle.com/terms-of-service")
+            agree.set("yes")
+        }
+        publishAlways()
+    }
+}
